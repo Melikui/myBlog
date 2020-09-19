f3a505357823213bc8f2927ce394ea8f45adb7ed
Melikui <melikui@qq.com>
2020-09-19 22:31:01 +0800

    在首页添加了一部分笔记

--- routers/main.js
+++ routers/main.js
@@ -19,17 +19,36 @@ router.use(function (req, res, next) {
 });
 
 
-
-
-
 // 首页
 router.get('/', function (req, res, next) {
+
+    data.category = req.query.category || '';
+    data.count = 0;
+    data.page = Number(req.query.page || 1);
+    data.limit = 5;
+    data.pages = 0;
+    var where = {};
+    if (data.category) {
+        where.category = data.category;
+    }
     // 读取所有的分类信息
-    Category.find().then(function (categories) {
-        res.render('main/index', {
-            userInfo: req.userInfo,
-            categories: categories
+    Content.where(where).count().then(function (count) {
+        data.count = count;
+        // 计算总页数
+        data.pages = Math.ceil(data.count / data.limit);
+        // 取值不能超过pages
+        data.page = Math.min(data.page, data.pages);
+        // 取值不能小于1
+        data.page = Math.max(data.page, 1);
+
+        var skip = (data.page - 1) * data.limit;
+        return Content.where(where).find().sort({ _id: -1 }).limit(data.limit).skip(skip).populate(['category', 'user']).sort({
+            addTime: -1
         });
+
+    }).then(function (contents) {
+        data.contents = contents;
+        res.render('main/index', data);
     });
 });
 
